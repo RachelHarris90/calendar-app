7cc53f681f03bfb69918112bc65f8e404a38fd54
Rachel Harris <rachel.harris@msn.com>
2022-01-15 16:08:17 +1100

    Loop through to set colour of each row based on current time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,7 @@ var currentDay = document.querySelector("#currentDay")
 currentDay.textContent = "Today is " + (moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // TODO: Update time every second
 // Identify which time has been selected
-var container = $('#container');
-var row = document.querySelector('.row')
+
 var hour = document.querySelector('.hour')
 var save = document.querySelector('.saveBtn')
 
@@ -33,16 +32,29 @@ save.addEventListener("click", saveEvent);
 // TODO: Loop through the rows of hours
 function checkHourStatus() {
     var currentHour = moment();
-    var rowHour = 9;
-    console.log(currentHour.hour());
-    console.log(rowHour);
-    if (rowHour < currentHour.hour()) {
-        row.setAttribute("class", "past");
-    } else if (rowHour == currentHour.hour()) {
-        row.setAttribute("class", "present");
-    } else {
-        row.setAttribute("class", "future");
-        return;
+
+    // Get count of number of hour rows on page
+    var containerCount = document.querySelector('#container').childElementCount;
+    console.log(containerCount)
+
+    // Get ID of each row then take the hour number
+    var row = document.querySelector(".row")
+
+    var container = document.querySelector("#container")
+    console.log(container.children)
+
+    // Loop through each row to apply styling based on hour of row
+    for (var i = 0; i < containerCount; i++) {
+        var rowHour = container.children[i].getAttribute("id").split("-")[1];
+        console.log(rowHour);
+        // Check if the hour of a row is past/present/future
+        if (rowHour < currentHour.hour()) {
+            container.children[i].setAttribute("class", "past");
+        } else if (rowHour == currentHour.hour()) {
+            container.children[i].setAttribute("class", "present");
+        } else {
+            container.children[i].setAttribute("class", "future");
+        }
     }
 };
 checkHourStatus();
