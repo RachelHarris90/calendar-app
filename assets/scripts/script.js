9b44fc449b4413a876bdda3b2dc07fd057dfec50
Rachel Harris <rachel.harris@msn.com>
2022-01-15 11:22:46 +1100

    continue to attempt to save input value based on button clicked

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,6 +7,7 @@ var container = $('#container');
 var row = document.querySelector('.row')
 var hour = document.querySelector('.hour')
 var eventText = $('input[name="event-text"]');
+var save = $('.saveBtn')
 
 // TODO: Get saved events of each row
 var savedEvents = [];
@@ -14,16 +15,20 @@ function getSavedEvents() {
     savedEvents = localStorage.getItem("eventText")
 }
 
-var enteredEvent = $(':input');
 // Save events to local storage
 // TODO: Save events text of each row
-function saveEvent(event) {
+
+function saveEvent(event, t) {
     event.preventDefault();
+    // Target the input field that is a sibling of the savebutton
+    console.log(event.target);
     $(this).siblings(event.target);
-    console.log($(this).siblings(event.target));
+    // Save the value of the eventText into local storage with a key of the ID of that row
     localStorage.setItem($(this).parent().attr('id'), eventText.val());
 };
+
 container.on('submit', saveEvent);
+
 
 // Set time past events to have class of .past
 // TODO: Loop through the rows of hours
